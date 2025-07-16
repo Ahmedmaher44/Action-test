daffd37b8d96b42edc0f32ea822dba71d3502175
ahmed maher <ahmedmaher2993@gmail.com>
2025-07-17 01:17:46 +0300

    some updates

--- Scr/test-ci/obj/Debug/net9.0/test-ci.AssemblyInfo.cs
+++ Scr/test-ci/obj/Debug/net9.0/test-ci.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-ci")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+988b3a514834c67d010629ab0521b05fed1fc294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269b1d55f60c5ae9e52d7f5940e751dceb4fba51")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-ci")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-ci")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
